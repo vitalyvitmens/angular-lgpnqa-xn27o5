ae27e80363b86a569376055769153e68038023ea
vitalyvitmens <89190308+vitalyvitmens@users.noreply.github.com>
2023-01-26 10:21:01 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-lgpnqa-xn27o5
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-lgpnqa-numiya)
---
+++ src/app/product-list/product-list.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+
+import { products } from '../products';
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css'],
+})
+export class ProductListComponent {
+  products = products;
+
+  share() {
+    window.alert('Продуктом поделились!');
+  }
+
+  onNotify() {
+    window.alert('Вы будете уведомлены, когда продукт поступит в продажу');
+  }
+}
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
